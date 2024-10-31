52005b1d5aced4a3ea1379491af85aba52f31a1a
Jared Devine <emailjareddevine@gmail.com>
2024-10-31 15:26:34 -0400

    changed config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jareddevine.com",
+  site: "https://example.com",
   integrations: [mdx(), sitemap(), tailwind()],
 });
